14bc502403fcd8781a2ed3dd3bfa6ee5fcfb9e94
Zicka67 <art.book@hotmail.fr>
2023-01-30 14:35:21 +0100

    Correction and creation of findAll function

--- db-functions.php
+++ db-functions.php
@@ -1,9 +1,9 @@
 <?php
 
-function connexion() {
 try
 {
-$db = new PDO(
+    // On se connecte à MySQL
+$sqlClient = new PDO(
     'mysql:host=localhost;dbname=store_gk;charset=utf8',
     'root',
     '',
@@ -16,25 +16,54 @@ $db = new PDO(
         // renverra les données sous forme de tableau associatif (FETCH_ASSOC).
     ]
 );
-}
-    //S'il y a une erreur, il rentre dans le bloc catch et fait ce qu'on lui demande (ici, on arrête l'exécution de la page en affichant un message décrivant l'erreur).
-    //Si au contraire tout se passe bien, PHP poursuit l'exécution du code et ne lit pas ce qu'il y a dans le bloc catch  . Votre page PHP ne devrait donc rien afficher pour le moment.
-    catch (Exception $e) 
+
+    } catch (Exception $e) 
     {
+        	// En cas d'erreur, on affiche un message et on arrête tout
         die('Erreur : ' . $e->getMessage());
     }
 
-}
+
 
 function findAll()
 {
-    $db = dbFunction();
+    $sqlClient = new PDO(
+        'mysql:host=localhost;dbname=store_gk;charset=utf8',
+        'root',
+        '',
+        [
+            
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, //Cette option précise le type d'erreur que PDO renverra en cas de requête invalide.
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC, //Cette définit le mode de récupération des données de la base par défaut. 
+            // Ici, PDO renverra les données sous forme de tableau associatif (FETCH_ASSOC).
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'set NAMES utf8' //Cette définit le mode de récupération des données de la base par défaut. Ici, PDO 
+            // renverra les données sous forme de tableau associatif (FETCH_ASSOC).
+        ]
+    );
 
-    $sqlQuery = 'SELECT * FROM product';
+    $sqlRequest = 'SELECT * FROM product';
+    $storeStatement = $sqlClient->prepare($sqlRequest);
+    $storeStatement->execute();
+    $store = $storeStatement->fetchAll(); //fetch=va chercher
+    return $store;
+
+    
+ }
+?>
    
-   
+<!-- 
+function findOneById($id) 
+{
+    // Celle-ci renverra le produit répondant à l'identifiant $id en paramètre.
+
 }
 
+function insertProduct($name, $descr, $price)
+{
+//     Cette dernière fonction insérera en base de données un nouvel enregistrement dans la table 
+// product doté du nom $name, de la description $descr et du prix $price souhaités en paramètres.
+
+} -->
 
 
 
@@ -54,4 +83,4 @@ function findAll()
 
 
 
-?>
+
