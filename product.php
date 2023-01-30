33919d5288ed3cc424993356d7c578fa75b7c546
Zicka67 <art.book@hotmail.fr>
2023-01-30 19:29:27 +0100

    Add include pour les functions + comment

--- product.php
+++ product.php
@@ -1,52 +1,77 @@
 <?php
-session_start();
+session_start(); //Cette ligne démarre une nouvelle session ou reprend une session existante pour cet utilisateur.
+include "nbProduit.php"; //Cette ligne inclut le fichier functions.php dans le code actuel.
+require_once('db-functions.php');//Cette ligne inclut le fichier db-functions.php dans le code actuel, mais qu'une seule fois.
+
+$product = findOneById($_GET['id']);
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
 
-    <link rel="stylesheet" href="style.css">
-
-    <title>Product</title>
-
+<title>Product</title>
 </head>
 
 <body>  
-    <div>
+<header>
+		<nav>
+			<a href="index.php">
+				<i class="fa fa-home"></i>
+			</a>
+			<ul>
+				<li>
+					<a href="index.php">INDEX</a>
+				</li>
+				<li>
+					<a href="recap.php">RÉCAP</a>
+				</li>
+			</ul>
+			<a href="recap.php" <?php if (nbProduits() == null || nbProduits() == 0) {
+									echo "style= 'display: none'";
+								} ?>>
+				<i class="fa fa-shopping-cart"></i>
+				<p id="shoppingCart"><?= nbProduits() ?></p>
+			</a>
+		</nav>
+	</header>
+	<main>
+		<div id="container">
+			<article id="productFocus">
+				<div id="productIMG">
+					<img src="<?= $product['img'] ?>" alt="">
+				</div>
+				<div id="productInfo">
+					
+					<h1><?= ucFirst($product['name']) ?></h1>
+					
+					<p id="productPrice"><?= $product['price'] ?> €</p>
+					
+					<p class='description'><?= ucFirst($product['description']) ?></p>
+					
+					<div id="actionProduct">
 
-    <?php
-        require_once('db-functions.php'); 
-        $product=findOneById ($_GET["id"]);  
-    ?>
-        <article class="container-product"> 
-                
-                        <div>
-                            <img class="resize" src="img\shutterstock_575378506 (1).jpg" alt=""> 
-                        </div>
+						<a href="index.php">Retour</a>
+						
+						<div>
+						<i class="fa fa-shopping-cart"></i>
+							<a href="traitement.php?action=addToCart&id=<?= $_GET['id'] ?>">Ajouter un produit</a>
+							
+						</div>
+					</div>
+				</div>
 
-                        <div>
-                            <p><?= $article['name']; ?> €</p>
-                            <p><?= $article['description']; ?> €</p>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere consectetur nihil totam quidem asperiores. Praesentium sit porro culpa ipsum velit consequatur dignissimos, alias reprehenderit illo tempora itaque cumque facilis! Corporis!</p>
-                            <p><?= $article['price']; ?> €</p>
-
-                            <form action="traitement.php?action=ajouterProduit" method="post">
-	                        <p>
-			                <input type="submit" name="submit" value="Ajouter le produit" class="p-3 mb-2 btn btn-outline-info">
-		                    </p>
-                            <a href="http://localhost/inserer_BDD/index.php">retour</a>
-                            <br><br>
-                        </div>
-                    </a>
-         </article>
-
-
-
+			</article>
+		</div>
+	</main>
 </body>
+
+
 
 </html>
